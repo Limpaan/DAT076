cceb27068743bcadacc7141cfd02e2525db00000
Viktor Lindblom <viktor.lindblom@gmail.com>
2019-02-11 11:12:16 +0100

    Started lab 3

---
+++ library/src/main/java/chl/hajo/library/dao/BookCatalogue.java
@@ -0,0 +1,20 @@
+package chl.hajo.library.dao;
+
+import javax.ejb.Stateless;
+
+/**
+ * All orders Responsible for putting new PurchaseOrders objects into the model
+ *
+ * @author hajo
+ */
+@Stateless
+public class BookCatalogue  {
+
+    
+    public BookCatalogue() {
+       
+    }
+
+ 
+   
+}
